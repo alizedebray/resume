9a0cf4f2f40e5370b8057cd6aa0d071820def5c9
alizedebray <alize.debray@wanadoo.fr>
2017-11-13 07:20:21 +0100

    Profile section creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './sections/home/home.component';
+import { ProfileComponent } from './sections/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sections/profile/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'profile-section',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.sass']
+})
+export class ProfileComponent {
+
+}
